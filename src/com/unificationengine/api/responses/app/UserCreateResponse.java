3c331ccb123785095cd39dff5ea0780504bcb27c
Hossam Saraya <hossam.saraya@gmail.com>
2016-03-30 14:40:46 +0200

    Response POJOs

---
+++ src/com/unificationengine/api/responses/app/UserCreateResponse.java
@@ -0,0 +1,7 @@
+package com.unificationengine.api.responses.app;
+
+/**
+ * Created by deadlock on 3/30/16.
+ */
+public class UserCreateResponse {
+}
---
+++ src/com/unificationengine/api/responses/connection/MessageSendResponse.java
@@ -0,0 +1,7 @@
+package com.unificationengine.api.responses.connection;
+
+/**
+ * Created by deadlock on 3/30/16.
+ */
+public class MessageSendResponse {
+}
