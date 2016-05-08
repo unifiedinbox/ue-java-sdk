6eef09148b74a01127d66448b3b6a7bc138bac4b
Hossam Saraya <hossam.saraya@gmail.com>
2016-05-08 10:46:28 +0200

    Update Readme

--- src/com/unificationengine/exceptions/UnificationEngineException.java
+++ src/com/unificationengine/exceptions/UnificationEngineException.java
@@ -24,4 +24,11 @@ public class UnificationEngineException extends Exception {
     public UnificationEngineException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
+
+
+    @Override
+    public String getMessage() {
+        System.out.println("From Exception Class");
+        System.out.println(this.getMessage());
+    }
 }
